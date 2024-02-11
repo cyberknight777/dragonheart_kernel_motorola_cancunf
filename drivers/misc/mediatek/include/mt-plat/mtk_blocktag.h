ebfb5b15d9f61495668a1f563f9e2deb487ebd0e
bengris32 <bengris32@protonmail.ch>
2024-02-11 22:57:47 +0000

    misc/mediatek: mt-plat: Fix build with blocktag disabled
    Change-Id: If934bc3adfaffa23d25ea2624f57862268cbf0a5
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/misc/mediatek/include/mt-plat/mtk_blocktag.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_blocktag.h
@@ -341,7 +341,7 @@ void mmc_mtk_biolog_clk_gating(bool gated);
 #define mmc_mtk_biolog_send_command(...)
 #define mmc_mtk_biolog_transfer_req_compl(...)
 #define mmc_mtk_biolog_check(...)
-#define mmc_mtk_biolog_init(...)
+inline int mmc_mtk_biolog_init(struct mmc_host *mmc) { return 0; }
 #define mmc_mtk_biolog_exit(...)
 #define set_mmc_perf_mode(...)
 #define mmc_mtk_biolog_clk_gating(...)
