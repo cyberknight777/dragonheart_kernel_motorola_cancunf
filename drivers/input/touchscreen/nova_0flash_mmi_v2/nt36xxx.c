8f3816ef6513397b90248815500ddcc31cd10828
Jhon Diaz <electimon@gmail.com>
2024-08-11 21:24:32 +0530

    nova_0flash_mmi_v2: Load firmware earlier
    * stupid code
    
    Signed-off-by: Jhon Diaz <electimon@gmail.com>
    Change-Id: I4bd5108efc438ad2cb4323c356aacfd8b2ba3ea4
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/input/touchscreen/nova_0flash_mmi_v2/nt36xxx.c
+++ drivers/input/touchscreen/nova_0flash_mmi_v2/nt36xxx.c
@@ -2933,7 +2933,7 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 	}
 	INIT_DELAYED_WORK(&ts->nvt_fwu_work, Boot_Update_Firmware);
 	// please make sure boot update start after display reset(RESX) sequence
-	queue_delayed_work(nvt_fwu_wq, &ts->nvt_fwu_work, msecs_to_jiffies(14000));
+	queue_delayed_work(nvt_fwu_wq, &ts->nvt_fwu_work, msecs_to_jiffies(7000));
 	ts->fw_delay_que = 1;
 #endif
 #ifdef LCM_FAST_LIGHTUP
