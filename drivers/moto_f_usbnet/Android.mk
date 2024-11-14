20c957ec001c5633897b643ab3be1489b12be5c4
Cyber Knight <cyberknight755@gmail.com>
2024-11-15 04:55:45 +0800

    drivers: Import moto usb drivers from `MMI-U1TDS34.94-12-7-6`
    - These drivers were imported from [1] and [2] respectively.
    
    [1]: https://github.com/MotorolaMobilityLLC/motorola-kernel-modules/tree/MMI-U1TDS34.94-12-7-6/drivers/moto_f_mass_storage
    
    [2]: https://github.com/MotorolaMobilityLLC/motorola-kernel-modules/tree/MMI-U1TDS34.94-12-7-6/drivers/moto_f_usbnet
    
    Change-Id: I7d82dc2f6e8de48581d132199e47de531cd95804
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

---
+++ drivers/moto_f_usbnet/Android.mk
@@ -0,0 +1,8 @@
+DLKM_DIR := vendor/motorola
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := moto_f_usbnet.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
