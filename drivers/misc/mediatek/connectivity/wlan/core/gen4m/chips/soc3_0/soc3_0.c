3d5919a05c3ef577bd8409f625958f5a2859a99d
Sarthak Roy <sarthakroy2002@gmail.com>
2025-09-14 21:26:59 +0530

    treewide: Shut more MTK build-time spams
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I7cde970abcd56297a51c9e9b7d285d3230c881c7
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/chips/soc3_0/soc3_0.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/chips/soc3_0/soc3_0.c
@@ -3198,7 +3198,6 @@ uint32_t soc3_0_wlanPowerOnDownload(
 		}
 
 #if (CFG_ROM_PATCH_NO_SEM_CTRL == 0)
-#pragma message("ROM code supports SEM-CTRL for ROM patch download")
 		if (wlanPatchIsDownloaded(prAdapter)) {
 			kalFirmwareImageUnmapping(prAdapter->prGlueInfo, NULL,
 						  prFwBuffer);
@@ -3206,8 +3205,6 @@ uint32_t soc3_0_wlanPowerOnDownload(
 				"No need to download patch\n");
 			return WLAN_STATUS_SUCCESS;
 		}
-#else
-#pragma message("ROM code supports no SEM-CTRL for ROM patch download")
 #endif
 
 		/* Patch DL */
